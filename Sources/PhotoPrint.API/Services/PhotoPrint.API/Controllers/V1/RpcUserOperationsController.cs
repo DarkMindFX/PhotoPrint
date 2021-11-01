69c2b67cc12c082c55fc9a87eebe3003e5eba00e
DarkMindFX <contact@darkmindfx.com>
2021-11-01 11:45:15 +0100

    Adding User RPC controller

---
+++ Sources/PhotoPrint.API/Services/PhotoPrint.API/Controllers/V1/RpcUserOperationsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using PPT.PhotoPrint.API.Filters;
+using PPT.Interfaces.Entities;
+using PPT.Utils.Convertors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using PPT.PhotoPrint.API.Helpers;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace PPT.PhotoPrint.API.Controllers.V1
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    [UnhandledExceptionFilter]
+    public class RpcUserOperationsController : BaseController
+    {
+
+        private readonly Dal.IUserDal _dalUser;
+        private readonly Dal.IContactDal _dalContact;
+        private readonly Dal.IUserContactDal _dalUserContact;
+        private readonly ILogger<UsersController> _logger;
+        private readonly IOptions<AppSettings> _appSettings;
+
+
+        public RpcUserOperationsController(Dal.IUserDal dalUser,
+                                        Dal.IContactDal dalContact,
+                                        Dal.IUserContactDal dalUserContact,
+                                        ILogger<UsersController> logger,
+                                        IOptions<AppSettings> appSettings)
+        {
+            _dalUser = dalUser;
+            _dalContact = dalContact;
+            _dalUserContact = dalUserContact;
+            _logger = logger;
+            _appSettings = appSettings;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login"), ActionName("Login")]
+        public IActionResult Login(DTO.LoginRequest dtoLogin)
+        {
+            _logger.LogTrace($"{System.Reflection.MethodInfo.GetCurrentMethod()} Started");
+
+            IActionResult response = null;
+
+            var existingEntity = _dalUser.GetAll().FirstOrDefault(u => u.Login.ToLower() == dtoLogin.Login.ToLower());
+            if (existingEntity != null)
+            {
+                string pwdHash = Helpers.PasswordHelper.GenerateHash(dtoLogin.Password, existingEntity.Salt);
+                if (pwdHash.Equals(existingEntity.PwdHash))
+                {
+                    var dtExpires = DateTime.Now.AddSeconds(_appSettings.Value.SessionTimeout);
+                    var sToken = GenerateToken(existingEntity, dtExpires);
+
+                    var dtoResponse = new DTO.LoginResponse()
+                    {
+                        User = UserConvertor.Convert(existingEntity, this.Url),
+                        Token = sToken,
+                        Expires = dtExpires
+                    };
+
+                    response = Ok(dtoResponse);
+                }
+                else
+                {
+                    response = Forbid();
+                }
+            }
+            else
+            {
+                response = NotFound($"User not found [login:{dtoLogin.Login}]");
+            }
+
+            _logger.LogTrace($"{System.Reflection.MethodInfo.GetCurrentMethod()} Ended");
+
+            return response;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register"), ActionName("Register")]
+        public IActionResult Register(DTO.RegisterRequest dtoRegister)
+        {
+            _logger.LogTrace($"{System.Reflection.MethodInfo.GetCurrentMethod()} Started");
+
+            IActionResult response = null;
+
+            // Inserting new user
+            var entityUser = UserConvertor.Convert(dtoRegister.User);
+            entityUser.Salt = Helpers.PasswordHelper.GenerateSalt(12);
+            entityUser.PwdHash = Helpers.PasswordHelper.GenerateHash(dtoRegister.User.Password, entityUser.Salt);
+
+            base.SetCreatedModifiedProperties(entityUser,
+                        "CreatedDate",
+                        null);
+
+            User newEntityUser = _dalUser.Insert(entityUser);
+
+            // Inserting user's contact
+            var entityContact = ContactConvertor.Convert(dtoRegister.Contact);
+            Contact newEntityContact = _dalContact.Insert(entityContact);
+            base.SetCreatedModifiedProperties(entityContact,
+                        "CreatedDate",
+                        null);
+            entityContact.CreatedByID = (long)entityUser.ID;
+
+            // Connecting user & contact
+            var entityUserContact = new PPT.Interfaces.Entities.UserContact();
+            entityUserContact.UserID = (long)newEntityUser.ID;
+            entityUserContact.ContactID = (long)newEntityContact.ID;
+            entityUserContact.IsPrimary = true;
+            UserContact newEntityUserContact = _dalUserContact.Insert(entityUserContact);
+
+            // Preparing response
+            response = StatusCode((int)HttpStatusCode.Created,
+                                    new DTO.RegisterResponse() { User = UserConvertor.Convert(newEntityUser, this.Url) });
+
+            _logger.LogTrace($"{System.Reflection.MethodInfo.GetCurrentMethod()} Ended");
+
+            return response;
+        }
+
+        #region Support methods
+        private string GenerateToken(Interfaces.Entities.User user, DateTime expires)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Value.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                    {
+                            new Claim("id", user.ID.ToString())
+                    }
+                ),
+                Expires = expires,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            string sToken = tokenHandler.WriteToken(token);
+
+            return sToken;
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/PhotoPrint.API/Tests/Test.PhotoPrint.API/Controllers/V1/TestRpcUserOperationsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PhotoPrint.Test.E2E.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.E2E.API.Controllers.V1
+{
+    public class TestRpcUserOperationsController : E2ETestBase, IClassFixture<WebApplicationFactory<PPT.PhotoPrint.API.Startup>>
+    {
+        public TestRpcUserOperationsController(WebApplicationFactory<PPT.PhotoPrint.API.Startup> factory) : base(factory)
+        {
+            _testParams = GetTestParams("GenericControllerTestSettings");
+        }
+
+        [Fact]
+        public void User_Register_Success()
+        {
+        }
+
+        [Fact]
+        public void User_Register_LoginAlreadyExists()
+        {
+        }
+
+        [Fact]
+        public void User_Register_EmailAlreadyExists()
+        {
+        }
+
+        [Fact]
+        public void User_Register_InvalidUserTypeID()
+        {
+        }
+
+        [Fact]
+        public void User_Register_InvalidUserStatusID()
+        {
+        }
+
+        [Fact]
+        public void User_Login_Success()
+        {
+        }
+
+        [Fact]
+        public void User_Login_InvalidLogin()
+        {
+        }
+
+
+        [Fact]
+        public void User_Login_InvalidPassword()
+        {
+        }
+
+        [Fact]
+        public void User_Login_InvalidEmpty()
+        {
+        }
+
+        [Fact]
+        public void User_Login_PasswordEmpty()
+        {
+        }
+    }
+}
