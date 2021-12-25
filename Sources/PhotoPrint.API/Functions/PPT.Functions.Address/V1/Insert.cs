2034ad0ac5f61f0c84add41038302287cb40c0f8
DarkMindFX <contact@darkmindfx.com>
2021-12-25 16:45:33 +0100

    Adding Address CRUD functions

---
+++ Sources/PhotoPrint.API/Functions/PPT.Functions.Address/V1/Insert.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PPT.Utils.Convertors;
+using PPT.Services.Common.Helpers;
+using PPT.Interfaces;
+using System.Net;
+
+namespace PhotoPrint.Functions.Address.V1
+{
+    public static class Insert
+    {
+        [FunctionName("AddressesInsert")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/addresses")] HttpRequest req,
+            ILogger log)
+        {
+            IActionResult result = null;
+            var funHelper = new PPT.Functions.Common.FunctionHelper();
+            log.LogInformation($"{System.Reflection.MethodInfo.GetCurrentMethod()} Started");
+
+            try
+            {
+                var dal = funHelper.CreateDal<IAddressDal>();
+
+                var content = await new StreamReader(req.Body).ReadToEndAsync();
+
+                var dto = JsonConvert.DeserializeObject<PPT.DTO.Address>(content);
+
+                var entity = AddressConvertor.Convert(dto);
+                
+                funHelper.SetCreatedModifiedProperties(entity,
+                            "CreatedDate",
+                            null);
+
+                PPT.Interfaces.Entities.Address newEntity = dal.Insert(entity);
+
+                if (newEntity != null)
+                {
+                    result = new ObjectResult(funHelper.ToJosn(AddressConvertor.Convert(newEntity, null)))
+                    {
+                        StatusCode = (int)HttpStatusCode.Created
+                    };
+                }
+                else
+                {
+                    result = new ObjectResult(funHelper.ToJosn(new PPT.DTO.Error()
+                    {
+                        Code = (int)HttpStatusCode.InternalServerError,
+                        Message = $"Something went wrong. Address was not inserted."
+                    }))
+                    {
+                        StatusCode = (int)HttpStatusCode.InternalServerError
+                    };
+                }
+
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString());
+            }
+
+            log.LogInformation($"{System.Reflection.MethodInfo.GetCurrentMethod()} Ended");
+
+            return result;
+        }
+    }
+}
---
+++ Sources/PhotoPrint.API/Functions/PPT.Functions.User/V1/Update.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PPT.Utils.Convertors;
+using System.Net;
+using PPT.Interfaces;
+using PPT.Services.Common.Helpers;
+
+namespace PhotoPrint.Functions.User.V1
+{
+    public static class Update
+    {
+        [FunctionName("UsersUpdate")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "v1/users")] HttpRequest req,
+            ILogger log)
+        {
+            IActionResult result = null;
+            var funHelper = new PPT.Functions.Common.FunctionHelper();
+            log.LogInformation($"{System.Reflection.MethodInfo.GetCurrentMethod()} Started");
+
+            try
+            {
+                var dal = funHelper.CreateDal<IUserDal>();
+
+                var content = await new StreamReader(req.Body).ReadToEndAsync();
+
+                var dto = JsonConvert.DeserializeObject<PPT.DTO.User>(content);
+
+                var newEntity = UserConvertor.Convert(dto);
+
+                var existingEntity = dal.Get(newEntity.ID);
+
+                if (existingEntity != null)
+                {
+                    if (!string.IsNullOrEmpty(dto.Password))
+                    {
+                        newEntity.PwdHash = PasswordHelper.GenerateHash(dto.Password, existingEntity.Salt);
+                    }
+
+                    newEntity.CreatedDate = existingEntity.CreatedDate;
+
+                    funHelper.SetCreatedModifiedProperties(newEntity,
+                                            "ModifiedDate",
+                                            "ModifiedByID");
+                    PPT.Interfaces.Entities.User entity = dal.Update(newEntity);
+
+                    result = new ObjectResult(funHelper.ToJosn(UserConvertor.Convert(newEntity, null)));
+                }
+                else
+                {
+                    result = new ObjectResult(funHelper.ToJosn(new PPT.DTO.Error()
+                    {
+                        Code = (int)HttpStatusCode.NotFound,
+                        Message = $"User was not found [ids:{newEntity.ID}]"
+                    }))
+                    {
+                        StatusCode = (int)HttpStatusCode.NotFound
+                    };
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString());
+            }
+
+            log.LogInformation($"{System.Reflection.MethodInfo.GetCurrentMethod()} Ended");
+
+            return result;
+        }
+    }
+}
