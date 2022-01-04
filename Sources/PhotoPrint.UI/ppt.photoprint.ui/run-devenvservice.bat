a191157c7d1df00ec77ad0b6e5106ced9bfdb88d
DarkMindFX <contact@darkmindfx.com>
2022-01-04 19:53:56 +0100

    Updating run bat files

--- Sources/PhotoPrint.UI/ppt.photoprint.ui/run-devenvservice.bat
+++ Sources/PhotoPrint.UI/ppt.photoprint.ui/run-devenvservice.bat
@@ -1,6 +1,6 @@
 
 docker rm ppt.photoprint.ui
 
-docker run --name ppt.photoprint.ui --add-host=ppt_photoprint_api:192.168.0.248 -it -p 3000:80 ^
+docker run --name ppt.photoprint.ui -it -p 3000:80 ^
 	-e "ENVIRONMENT=dev"^
 	globus000/ppt.photoprint.ui
